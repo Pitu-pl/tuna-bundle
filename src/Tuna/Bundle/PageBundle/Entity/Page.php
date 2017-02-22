91057ae01935156b9db887a9a266184ad784e191
Adam Szajuk <adam.szajuk@thecodeine.com>
2017-02-22 09:26:35 +0100

    TUNACMS-109 abstraction for entities

--- src/Tuna/Bundle/PageBundle/Entity/Page.php
+++ src/Tuna/Bundle/PageBundle/Entity/Page.php
@@ -4,20 +4,13 @@ namespace TheCodeine\PageBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
 use TunaCMS\PageComponent\Model\Page as BasePage;
 
 /**
  * Page
  *
- * @ORM\Table(name="pages")
- * @ORM\Entity(repositoryClass="TheCodeine\PageBundle\Entity\PageRepository")
- * @ORM\EntityListeners({"TheCodeine\PageBundle\EventListener\PageAliasListener"})
- * @ORM\MappedSuperclass()
- * @ORM\HasLifecycleCallbacks
- *
- * @Gedmo\TranslationEntity(class="TheCodeine\PageBundle\Entity\PageTranslation")
+ * @ORM\MappedSuperclass
  */
 class Page extends BasePage
 {
